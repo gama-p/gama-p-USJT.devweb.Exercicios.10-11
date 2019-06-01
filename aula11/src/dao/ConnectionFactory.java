d6266a6a7eefea350dcd916245075b5f550eca0f
Paula Gama <gama.p@outlook.com>
2019-06-01 16:00:59 -0300

    Exercicios 10 e 11

---
+++ aula11/src/dao/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private static final ThreadLocal<Connection> conn = new ThreadLocal<>();
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public static Connection obtemConexao() throws SQLException {
+		if (conn.get() == null){
+			conn.set(DriverManager
+				.getConnection("jdbc:mysql://localhost/vendas?user=Alunos&password=alunos&useTimezone=true&serverTimezone=UTC"));
+	}
+		return conn.get();
+	}
+	public static void fecharConexao() throws SQLException {
+		if(conn.get() != null){
+			conn.get().close();
+			conn.set(null);
+		}
+	}
+}
